a17ef055c62e0d6e1776d49783de1edc8deb758c
dudamollica <dudarenno@yahoo.com.br>
2023-01-20 16:18:28 -0300

    Gerando token

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv"
 import bcrypt from "bcrypt"
+import joi from "joi"
+import {v4 as uuidv4} from "uuid"
 dotenv.config();
 
 const server = express();
@@ -22,9 +24,21 @@ server.use(express.json());
 server.listen(5000, () => console.log("Servidor Funfou"));
 
 server.post("/cadastro", async (req, res) => {
-  const { name, email, password } = req.body;
-  const passwordHashed = bcrypt.hashSync(password, 10)
+  const { name, email, password, confirmPassword } = req.body;
 
+  const usuarioSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+    confirmPassword: joi.string().valid(joi.ref("password")).required()
+  });
+  const {error} = usuarioSchema.validate({name, email, password, confirmPassword})
+  if (error){
+    const errorMessage = error.details.map(err=>err.message)
+    return res.status(422).send(errorMessage)
+  }
+
+  const passwordHashed = bcrypt.hashSync(password, 10)
   try {
     await db.collection("users").insertOne({ name, email, password: passwordHashed });
     res.sendStatus(201);
@@ -35,10 +49,29 @@ server.post("/cadastro", async (req, res) => {
 
 server.post("/", async (req, res) => {
   const { email, password } = req.body;
-
+  const token = uuidv4()
+  
   try {
-   
+   const checkEmail = await db.collection("users").findOne({email})
+   if(!checkEmail) return res.status(404).send("Email ou senha invalidos")
+   const checkPassword = bcrypt.compareSync(password, checkEmail.password)
+   if(!checkPassword) return res.status(404).send("Email ou senha invalidos")
+
+   res.send(token)
+
   } catch (error) {
     res.status(500).send(error.message);
   }
 });
+
+server.get("/home", (req, res)=>{
+
+})
+
+server.post("/nova-entrada", (req, res)=>{
+  
+})
+
+server.post("/nova-saida", (req, res)=>{
+  
+})
